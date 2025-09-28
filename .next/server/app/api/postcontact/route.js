a4e8a8dec45438114286c92963eb1ea84b9d4a37
gitking-han <hanzalarehman1000@gmail.com>
2025-09-27 21:51:57 -0700

    Delete .next directory

--- .next/server/app/api/postcontact/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/postcontact/route.js")
-R.c("server/chunks/node_modules_4676691e._.js")
-R.c("server/chunks/[root-of-the-server]__60ded298._.js")
-R.m("[project]/.next-internal/server/app/api/postcontact/route/actions.js [app-rsc] (server actions loader, ecmascript)")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/postcontact/route.js [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/postcontact/route.js [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
